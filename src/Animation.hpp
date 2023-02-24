3a0ce683587f8f70bfdf725e3cc6d924ab1f5eba
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-02-24 11:55:09 +0100

    ADD: Animation class for the download bar

--- src/Animation.hpp
+++ src/Animation.hpp
@@ -5,12 +5,28 @@
 #ifndef ALX_CHECKER_ANIMATION_HPP
 #define ALX_CHECKER_ANIMATION_HPP
 
+#include <iostream>
+#include <chrono>
+#include <thread>
 
+namespace alx
+{
+	class Animation {
 
-class Animation {
+	private:
+		int delay_;
+		mutable int width_;
+		mutable bool finished_ = false;
 
-};
+	public:
+		Animation(int width = 50, int delay = 100);
+		~Animation();
 
+		void start() const;
+		void stop() const;
+
+	};
+}
 
 
 #endif //ALX_CHECKER_ANIMATION_HPP
