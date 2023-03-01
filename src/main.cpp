ae1570931fa758c068d6b6e5a4097df2208a7b59
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-03-01 15:03:59 +0100

    EDIT: Testing

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,7 @@
  * @date 2020-05-01
  */
 
-#include "../inc/main.hpp"
-#include "config.hpp"
+#include "main.hpp"
 
 #include <iostream>
 #include <string>
@@ -15,23 +14,13 @@
 #include <chrono>
 #include <cstdlib>
 
-void spinningAnimation(int status) {
-    char spinChars[] = {'|', '/', '-', '\\'};
-    int i = 0;
-    while (!WIFEXITED(status)) {
-        std::cout << "\r[ " << spinChars[i % 4] << " ] Loading... ";
-        std::cout.flush();
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        i++;
-    }
-}
-
 int main(int ac, char **av) {
 
     (void) ac;
     (void) av;
 
     try {
+		std::cout << "ALX_CHECKER_VERSION: " << ALX_CHECKER_VERSION << std::endl;
 
 		alx::Checker checker;
 
