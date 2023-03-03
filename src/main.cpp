5be8ef9aa2cbc246aaf1d5b2c99b7a7327d414b6
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-03-03 20:57:03 +0100

    EDIT: TEST

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,8 @@ int main(int ac, char **av) {
 
 		alx::Checker checker(--ac, ++av);
 
+		checker.checkProject();
+
     } catch (const std::exception &e) {
         std::cerr << "[" << RED << "ERROR" << END << "]: " << e.what() << std::endl;
     }
