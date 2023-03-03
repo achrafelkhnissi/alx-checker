c3da2f024f1e35523601e0365e3157d1df8b6ec4
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-03-03 10:13:24 +0100

    test

--- src/main.cpp
+++ src/main.cpp
@@ -15,19 +15,11 @@
 
 int main(int ac, char **av) {
 
-	(void)ac;
-	(void)av;
-
     try {
 		std::cout << "ALX_CHECKER_VERSION: " << ALX_CHECKER_VERSION << std::endl;
 
-//		alx::Checker checker(--ac, ++av);
+		alx::Checker checker(--ac, ++av);
 
-		if (status != 0) {
-			std::cerr << "[" << RED << "ERROR" << END << "]: " << "Failed to move the executable to /usr/local/bin" << std::endl;
-			return EXIT_FAILURE;
-		}
-		*/
     } catch (const std::exception &e) {
         std::cerr << "[" << RED << "ERROR" << END << "]: " << e.what() << std::endl;
     }
