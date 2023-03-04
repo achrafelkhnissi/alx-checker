c9a42e65b076daf7fcebcebf60f762216e98ce20
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-03-04 23:41:20 +0100

    edit: modifying the output

--- src/checker/checkerUtils.cpp
+++ src/checker/checkerUtils.cpp
@@ -123,19 +123,19 @@ namespace alx
 			endsWithNewLine = false;
 
 
-		std::string result = (newLineCount == 2) ? "OK" : "KO";
-		std::cout << "Contains 2 lines\t: " << ((newLineCount == 2) ? "OK" : "KO") << std::endl;
-
-		
-		result = (shebang) ? "OK" : "KO";
-		std::cout << "Contains a Shebang\t: " << result << std::endl;
-
-		result = (endsWithNewLine) ? "OK" : "KO";
-		std::cout << "Ends with a newline\t: " << result << std::endl;
-
-		result = (hasSpecialChars) ? "KO" : "OK";
-		std::cout << "Has forbidden character\t: " << result << std::endl;
-
+//		std::string result = (newLineCount == 2) ? "OK" : "KO";
+//		std::cout << "Contains 2 lines\t: " << ((newLineCount == 2) ? "OK" : "KO") << std::endl;
+//
+//
+//		result = (shebang) ? "OK" : "KO";
+//		std::cout << "Contains a Shebang\t: " << result << std::endl;
+//
+//		result = (endsWithNewLine) ? "OK" : "KO";
+//		std::cout << "Ends with a newline\t: " << result << std::endl;
+//
+//		result = (hasSpecialChars) ? "KO" : "OK";
+//		std::cout << "Has forbidden character\t: " << result << std::endl;
+//
 		return (newLineCount == 2 && shebang && endsWithNewLine && !hasSpecialChars);
 	}
 
